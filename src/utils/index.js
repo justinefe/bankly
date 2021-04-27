cb478004fc900270ef02f0a8150bbb0827bf6867
Justin <efejustin3@gmail.com>
2021-04-27 17:22:35 +0100

    thired commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,8 +8,8 @@ export const saveToLocalStorage = (debit) => {
   }
 };
 const patterns = {
-  fullName: /^[A-Za-z]{3,}$/g,
-  cardNumber: /^[0-9]{16}$/,
+  fullName: /[A-Za-z s]{3,}/g,
+  cardNumber: /[0-9]{16}/,
   mm: /^(0[1-9]|10|11|12)$/g,
   yy: /^202[0-9]$/g,
   cvv: /^[0-9]{3}$/g,
